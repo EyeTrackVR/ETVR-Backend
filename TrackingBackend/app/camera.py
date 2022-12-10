530d55915f90e70580d1399269426621f4bb1a55
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2022-12-10 21:31:01 +0200

    Camera manages its own threads now

--- TrackingBackend/app/camera.py
+++ TrackingBackend/app/camera.py
@@ -14,18 +14,51 @@ class CameraState(Enum):
 
 
 class Camera:
-    def __init__(self, config: CameraConfig, out_queue: queue.Queue, cancellation_event: threading.Event):
+    def __init__(self, config: CameraConfig, image_queue: queue.Queue):
         self.config = config
-        self.cancellation_event = cancellation_event
         self.camera_state = CameraState.DISCONNECTED
         self.current_capture_source = self.config.capture_source
-        self.out_queue = out_queue
         self.camera: cv2.VideoCapture = None
+        # Threading stuff
+        self.cancellation_event: threading.Event = threading.Event()
+        self.thread: threading.Thread = threading.Thread()  # cant be set to None because we call methods on it
+        self.image_queue: queue.Queue = image_queue
 
-    def run(self) -> None:
+    def start(self) -> None:
+        # don't start a thread if one already exists
+        if self.thread.is_alive():
+            logger.debug(f"Thread requested to start but is already running")
+            return
+
+        logger.info("Starting Capture thread")
+        # clear cancellation event incase thread was stopped in the past
+        self.cancellation_event.clear()
+        # We need to recreate the thread because it is not possible to start a thread that has already been stopped
+        # MultiProcessing might have a way to do this in a better way????
+        self.thread = threading.Thread(target=self.__run, name="Capture")
+        self.thread.start()
+
+    def stop(self) -> None:
+        # can't kill a non-existent thread
+        if not self.thread.is_alive():
+            logger.debug("Request to kill dead thread was made!")
+            return
+
+        logger.info("Stopping Capture thread")
+        self.cancellation_event.set()
+        self.thread.join(timeout=5)
+        self.camera.release()
+        # If the thread fails to stop, start yelling at the top of your lungs and happy debugging!
+        if self.thread.is_alive():
+            logger.error(f"Failed to stop Capture thread!!!!!!!!")
+
+    def restart(self) -> None:
+        self.stop()
+        self.start()
+
+    def __run(self) -> None:
         while True:
             if self.cancellation_event.is_set():
-                logger.info("Exiting capture thread")
                 return
             # If things aren't open, retry until they are. Don't let read requests come in any earlier
             # than this, otherwise we can deadlock ourselves.
@@ -33,7 +66,6 @@ class Camera:
                 if self.camera_state == CameraState.DISCONNECTED or self.current_capture_source != self.config.capture_source:
                     self.connect_camera()
                 else:
-                    # TODO: if log is full of warning just add the capture event back and check it before this function
                     self.get_camera_image()
             else:  # no capture source is defined yet, so we wait :3
                 self.camera_state = CameraState.DISCONNECTED
@@ -42,36 +74,40 @@ class Camera:
         try:
             self.camera_state = CameraState.CONNECTING
             self.current_capture_source = self.config.capture_source
-            # try open the camera
             self.camera = cv2.VideoCapture(self.current_capture_source)
-            # check if we opened it
+            self.camera.setExceptionMode(True)
             if self.camera.isOpened():
                 self.camera_state = CameraState.CONNECTED
                 logger.info("Camera connected!")
                 return
             self.camera_state = CameraState.DISCONNECTED
             logger.info(f"Capture source {self.current_capture_source} not found, retrying")
-        except cv2.error:
+        except (cv2.error, Exception):
             logger.exception("Something is very broken")
 
     def get_camera_image(self) -> None:
+        # Be warned this is fucked beyond comprehension, if the capture source is dropped `self.camera.read()` wont
+        # return for a very long time essentially soft lock the thread for around 30 seconds each time it is called
+        # as far as I can tell our code is fine and that this is most likely a bug within OpenCV itself...
+        # A dirty hack to fix this might be to just ping the host to see if it is alive before retrieving a new frame
         try:
-            ret, image = self.camera.read()
+            ret, frame = self.camera.read()
             if not ret:
                 self.camera.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                logger.error("Problem getting frame")
+                logger.exception("Capture source problem, assuming camera disconnected, waiting for reconnect.")
+                self.camera_state = CameraState.DISCONNECTED
                 return
             frame_number = self.camera.get(cv2.CAP_PROP_POS_FRAMES)
             fps = self.camera.get(cv2.CAP_PROP_FPS)
-            self.push_image_to_queue(image, frame_number, fps)
+            self.push_image_to_queue(frame, frame_number, fps)
         except (cv2.error, Exception):
-            logger.warning("Capture source problem, assuming camera disconnected, waiting for reconnect.")
             self.camera_state = CameraState.DISCONNECTED
+            logger.exception("Failed to retrieve or push frame to queue")
 
-    def push_image_to_queue(self, image, frame_number, fps) -> None:
+    def push_image_to_queue(self, frame, frame_number, fps) -> None:
         # If there's backpressure, just yell. We really shouldn't have this unless we start getting
         # some sort of capture event conflict though.
-        qsize = self.out_queue.qsize()
+        qsize = self.image_queue.qsize()
         if qsize > 1:
             logger.warning(f"CAPTURE QUEUE BACKPRESSURE OF {qsize}. CHECK FOR CRASH OR TIMING ISSUES IN ALGORITHM.")
-        self.out_queue.put(image, frame_number, fps)
+        self.image_queue.put(frame, frame_number, fps)
