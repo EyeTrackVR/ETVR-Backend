f284f7cbd01ff4b8d6709edf324c672c201bb8a7
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-03-26 00:19:54 +0200

    Fixed PyInstalller

--- Makefile
+++ Makefile
@@ -6,7 +6,12 @@ install:
 run:
 	cd TrackingBackend/ && poetry run uvicorn main:app --reload
 
+pyinstaller:
+	poetry run pyinstaller ETVR.spec TrackingBackend/main.py
+
 clean:
+	rm tarcker-config.json
+	rm TrackingBackend/tacker-config.json
 	rm -rf TrackingBackend/__pycache__/
 	rm -rf TrackingBackend/app/__pycache__/
 	rm -rf TrackingBackend/app/algorithms/__pycache__/
--- TrackingBackend/main.py
+++ TrackingBackend/main.py
@@ -14,3 +14,10 @@ app.include_router(etvr_app.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    # since we should only be running this file directly once compiled with pyinstaller we shouldnt need to worry about 
+    # the reload flag because relisticly we wont be changing the code once compiled.
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
