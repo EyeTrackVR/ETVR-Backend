23396bd2a68cffb4f4853bdedf77676d09ed4ebf
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-03-26 19:20:52 +0100

    fix: remove websocket handle
    - remove the websocket handle as this will be implemented by Assassin

--- TrackingBackend/main.py
+++ TrackingBackend/main.py
@@ -11,33 +11,14 @@ app = FastAPI()
 app.include_router(etvr_app.router)
 
 
-@app.get("/hello")
+@app.get("/")
 async def root():
     return {"message": "Hello World!"}
 
 
-@app.websocket("/")
-async def websocket_endpoint(websocket: WebSocket):
-    print("[FastAPI WebSocket]: Accepting client connection...")
-    await websocket.accept()
-    while True:
-        try:
-            # Wait for any message from the client
-            await websocket.receive_text()
-            # Send message to the client
-            # resp = {'value': random.uniform(0, 1)}
-            # await websocket.send_json(resp)
-
-            # grab camera output and send over websocket
-
-        except Exception as e:
-            print("error:", e)
-            break
-    print("Bye..")
-
-
 if __name__ == "__main__":
     import uvicorn
-    # since we should only be running this file directly once compiled with pyinstaller we shouldnt need to worry about 
-    # the reload flag because relisticly we wont be changing the code once compiled.
+
+    # since we should only be running this file directly once compiled with pyinstaller we shouldnt need to worry about
+    # the reload flag because realistically we wont be changing the code once compiled.
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
