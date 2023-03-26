e0c7f951d66fae6c9a8d9442c4a737c3267af515
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-03-26 23:23:01 +0200

    Added Visualizer

--- TrackingBackend/app/visualizer.py
+++ TrackingBackend/app/visualizer.py
@@ -1,6 +1,26 @@
 import cv2
 from queue import Queue
+from fastapi.responses import StreamingResponse
+
 
 class Visualizer:
-    def __init__(self, image_queue: Queue,):
+    def __init__(self, image_queue: Queue, remove_from_queue: bool = False):
         self.image_queue: Queue = image_queue
+        self.remove_from_queue: bool = remove_from_queue
+
+    async def gen_frame(self):
+        while True:
+            if self.remove_from_queue:
+                frame = self.image_queue.get()
+            else:
+                frame = self.image_queue.queue[0]
+
+            ret, frame = cv2.imencode(".jpg", frame)
+            if not ret:
+                continue
+            frame = frame.tobytes()
+
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+    async def video_feed(self):
+        return StreamingResponse(self.gen_frame(), media_type="multipart/x-mixed-replace; boundary=frame")
--- TrackingBackend/main.py
+++ TrackingBackend/main.py
@@ -1,43 +1,19 @@
 from app.logger import setup_logger
 from app.etvr import ETVR
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, StreamingResponse
+
 
 setup_logger()
-
 
 etvr_app = ETVR()
 etvr_app.add_routes()
 app = FastAPI()
 app.include_router(etvr_app.router)
 
-
-@app.get("/hello")
-async def root():
-    return {"message": "Hello World!"}
-
-
-@app.websocket("/")
-async def websocket_endpoint(websocket: WebSocket):
-    print("[FastAPI WebSocket]: Accepting client connection...")
-    await websocket.accept()
-    while True:
-        try:
-            # Wait for any message from the client
-            await websocket.receive_text()
-            # Send message to the client
-            # resp = {'value': random.uniform(0, 1)}
-            # await websocket.send_json(resp)
-
-            # grab camera output and send over websocket
-
-        except Exception as e:
-            print("error:", e)
-            break
-    print("Bye..")
-
-
 if __name__ == "__main__":
     import uvicorn
-    # since we should only be running this file directly once compiled with pyinstaller we shouldnt need to worry about 
+
+    # since we should only be running this file directly once compiled with pyinstaller we shouldnt need to worry about
     # the reload flag because relisticly we wont be changing the code once compiled.
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
