9a41a7f6c693e7295eb18a6e5ce07a61ed518c72
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-05-05 19:42:25 +0200

    Visualizer is no longer blocks the event loop

--- TrackingBackend/app/visualizer.py
+++ TrackingBackend/app/visualizer.py
@@ -1,7 +1,9 @@
 import cv2
 from queue import Queue
 from fastapi.responses import StreamingResponse
+import asyncio
 
+OFLINE_IMAGE = cv2.imread("assets/CameraOffline.png")
 
 class Visualizer:
     def __init__(self, image_queue: Queue, remove_from_queue: bool = False):
@@ -11,7 +13,7 @@ class Visualizer:
     async def gen_frame(self):
         while True:
             if self.remove_from_queue:
-                frame = self.image_queue.get()
+                frame = self.image_queue.get(block=False)
             else:
                 frame = self.image_queue.queue[0]
 
@@ -19,8 +21,10 @@ class Visualizer:
             if not ret:
                 continue
             frame = frame.tobytes()
-
             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+            # sleep so we dont block the event loop
+            await asyncio.sleep(0.1)
+
 
     async def video_feed(self):
         return StreamingResponse(self.gen_frame(), media_type="multipart/x-mixed-replace; boundary=frame")
