4e84efc67ad0d7835a79b640af75be0ef51d7ca2
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-07-03 13:33:58 +0200

    Logger now has colors

--- TrackingBackend/app/logger.py
+++ TrackingBackend/app/logger.py
@@ -1,23 +1,17 @@
-from logging import StreamHandler
 from .types import LogLevel
 import logging
 import inspect
-import sys
+import coloredlogs
 
 
 def setup_logger() -> None:
     logger = logging.getLogger()
-    # TODO: should probably load this from config or have it be set with a parameter
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(levelname)s || %(processName)s[%(process)d] --> %(name)s<[%(filename)s:%(lineno)d, %(funcName)s()]> :: %(message)s"
+    # TODO: should probably load this from config or have it be set with a env var
+    coloredlogs.install(
+        level="DEBUG",
+        logger=logger,
+        fmt="%(levelname)s || %(processName)s[%(process)d] --> %(name)s<[%(filename)s:%(lineno)d],%(funcName)s()> :: %(message)s",
     )
-    # Create the Handler for logging data to console
-    console_handler = StreamHandler(sys.stdout)
-    console_handler.setFormatter(formatter)
-    # add handlers
-    logger.addHandler(console_handler)
-
 
 def set_log_level(level: LogLevel) -> None:
     logger = logging.getLogger()
@@ -31,5 +25,4 @@ def get_logger(name: str = "") -> logging.Logger:
         name = inspect.getmodule(frm[0]).__name__  # type: ignore
 
     logger = logging.getLogger(name)
-    logger.debug("Initialized logger for %s", name)
     return logger
