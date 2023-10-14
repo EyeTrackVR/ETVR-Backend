daab001d82956dcac96beaf032c9089ce6b029cd
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-10-14 19:20:50 +0200

    refactor: moved `OneEuroFilter` to its own file

---
+++ TrackingBackend/app/utils/misc_utils.py
@@ -0,0 +1,29 @@
+from app.types import EyeData, TRACKING_FAILED
+from queue import Queue, Empty
+import cv2
+
+
+def clamp(x, low, high):
+    return max(low, min(x, high))
+
+
+def clear_queue(queue: Queue) -> None:
+    while True:
+        try:
+            queue.get(block=True, timeout=0.1)
+        except Empty:
+            break
+
+
+# Base class for all algorithms
+class BaseAlgorithm:
+    # all algorithms must implement this method
+    def run(self, frame: cv2.Mat) -> EyeData:
+        return TRACKING_FAILED
+
+    def normalize(self, x: float, y: float, width: int, height: int) -> tuple[float, float]:
+        """takes a point and normalizes it to a range of 0 to 1"""
+        tx: float = x / width
+        ty: float = y / height
+
+        return tx, ty
