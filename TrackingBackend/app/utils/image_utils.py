ad6f367731af4d180317a7e9906ec22832d6fe7f
[Assassin] <49711232+ShyAssassin@users.noreply.github.com>
2024-02-24 19:27:58 +0200

    Added safe_crop to utils

--- TrackingBackend/app/utils/image_utils.py
+++ TrackingBackend/app/utils/image_utils.py
@@ -2,6 +2,16 @@ import cv2
 from cv2.typing import MatLike
 
 
+def safe_crop(frame: MatLike, x: int, y: int, w: int, h: int, keepsize=False):
+    frame_h, frame_w = frame.shape[:2]
+    outframe = frame[max(0, y) : min(frame_h, h), max(0, x) : min(frame_w, w)].copy()
+    reqsize_x, reqsize_y = abs(w - x), abs(h - y)
+    if keepsize and outframe.shape[:2] != (reqsize_y, reqsize_x):
+        # If the size is different from the expected size (smaller by the amount that is out of range)
+        outframe = cv2.resize(outframe, (reqsize_x, reqsize_y))
+    return outframe
+
+
 def mat_crop(x: int, y: int, w: int, h: int, frame: MatLike) -> MatLike:
     if x <= 0 or y <= 0 or w <= 0 or h <= 0:
         return frame
